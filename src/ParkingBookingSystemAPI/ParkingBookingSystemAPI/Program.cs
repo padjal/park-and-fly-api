c206c0ed3b7cfb3c2a7dcbb4dfeb1693fc0530bb
padjal <padjal136@gmail.com>
2024-04-08 18:18:21 +0300

    Refactor docker support

--- src/ParkingBookingSystemAPI/ParkingBookingSystemAPI/Program.cs
+++ src/ParkingBookingSystemAPI/ParkingBookingSystemAPI/Program.cs
@@ -1,11 +1,7 @@
-using Auth0.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Google;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using ParkingBookingSystemAPI.Models;
-using System.Configuration;
 
 namespace ParkingBookingSystemAPI
 {
@@ -17,7 +13,7 @@ namespace ParkingBookingSystemAPI
 
             //Real db
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseNpgsql(builder.Configuration.GetConnectionString("ApplicationContext")));
+                options.UseNpgsql(builder.Configuration.GetConnectionString("RemoteApplicationContext")));
 
             builder.Services.AddAuthorization();
 
@@ -65,14 +61,19 @@ namespace ParkingBookingSystemAPI
 
             app.MapIdentityApi<ApplicationUser>();
 
+            app.Ensure
+
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            /*if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            }*/
 
-            app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            //app.UseHttpsRedirection();
 
             if (!app.Environment.IsDevelopment())
             {
