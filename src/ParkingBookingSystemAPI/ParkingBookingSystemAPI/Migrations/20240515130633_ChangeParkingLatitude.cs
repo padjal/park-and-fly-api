62e56badc2f2c919960534e12d91583e47935f4b
padjal <padjal136@gmail.com>
2024-05-15 16:07:12 +0300

    Change parking latitute to latitude

---
+++ src/ParkingBookingSystemAPI/ParkingBookingSystemAPI/Migrations/20240515130633_ChangeParkingLatitude.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParkingBookingSystemAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeParkingLatitude : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Longitute",
+                table: "Parkings",
+                newName: "Longitude");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Longitude",
+                table: "Parkings",
+                newName: "Longitute");
+        }
+    }
+}
--- src/ParkingBookingSystemAPI/ParkingBookingSystemAPI/Models/Parking.cs
+++ src/ParkingBookingSystemAPI/ParkingBookingSystemAPI/Models/Parking.cs
@@ -13,7 +13,7 @@ namespace ParkingBookingSystemAPI.Models
         public string City { get; set; }
         public string Country { get; set; }
         public double Latitude { get; set; }
-        public double Longitute { get; set; }
+        public double Longitude { get; set; }
         public string Phone { get; set; }
         public int MaxCars { get; set; }
         public ICollection<Reservation> Reservations { get; set; }
